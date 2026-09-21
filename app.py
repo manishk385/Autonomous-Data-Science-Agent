cbd6a73143c209d35c5507710d3199ba9b51f0cd
manishk385 <manishkumaravel@gmail.com>
2026-09-21 13:39:47 +0530

    Update API configuration

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import requests
 import pandas as pd
+import os
 
 st.set_page_config(page_title="DSmith AI", page_icon="🤖", layout="wide")
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv("API_URL")
 
 st.markdown("""
 <style>
